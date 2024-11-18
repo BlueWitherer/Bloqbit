8adfda38d728d26475b3c526fddfe2f538758aec
Cheeseworks <bluewitherer2422@gmail.com>
2024-11-18 12:00:50 -0500

    fix eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,7 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { languageOptions: { globals: globals.node } },
-  pluginJs.configs.recommended,
+  { languageOptions: { globals: globals.browser } },
 ];
